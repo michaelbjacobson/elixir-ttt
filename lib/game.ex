77d96d3ccd7fe276c9b1669e2a696ac7cbc83ecd
Michael Jacobson <mjacobson@8thlight.com>
2018-05-02 15:29:41 +0100

    • add functions to Game module • add function to switch players on given game struct • add function to update board map on given game struct • refactor test for Game.new/3 • all tests passing

--- lib/game.ex
+++ lib/game.ex
@@ -1,4 +1,5 @@
 defmodule TicTacToe.Game do
+  alias TicTacToe.Board
 
   defstruct [
     :board,
@@ -9,13 +10,25 @@ defmodule TicTacToe.Game do
   ]
 
   def new(board, player_1, player_2) do
-  %TicTacToe.Game {
-    board: board,
-    player_1: player_1,
-    player_2: player_2,
-    current_player: player_1,
-    is_over?: false
-  }
-end
+    %TicTacToe.Game {
+      board: board,
+      player_1: player_1,
+      player_2: player_2,
+      current_player: player_1,
+      is_over?: false
+    }
+  end
+
+  def switch_players(game) do
+    cond do
+      game.current_player == game.player_1 -> %{ game | current_player: game.player_2}
+      game.current_player == game.player_2 -> %{ game | current_player: game.player_1}
+    end
+  end
+
+  def update_board(game, move_index) do
+    updated_board = Board.update(game.board, move_index, game.current_player.marker)
+    %{ game | board: updated_board }
+  end
 
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -1,16 +1,34 @@
 defmodule GameTest do
   use ExUnit.Case
   alias TicTacToe.Game
+  alias TicTacToe.Board
+  alias TicTacToe.Player
 
   test "Game.new/3 returns a new Game struct populated with the arguments given" do
     expected = %TicTacToe.Game{
-      board: :board,
-      current_player: :first_player,
+      board: %{1 => "1", 2 => "2", 3 => "3", 4 => "4", 5 => "5", 6 => "6", 7 => "7", 8 => "8", 9 => "9"},
+      current_player: %TicTacToe.Player{io: IO, marker: "X"},
       is_over?: false,
-      player_1: :first_player,
-      player_2: :second_player
+      player_1: %TicTacToe.Player{io: IO, marker: "X"},
+      player_2: %TicTacToe.Player{io: IO, marker: "O"}
     }
-    actual = Game.new(:board, :first_player, :second_player)
+    actual = Game.new(Board.new, Player.new("X"), Player.new("O"))
+    assert actual == expected
+  end
+
+  test "Game.switch_players/1 returns a new Game struct with the current player field updated" do
+    expected = "O"
+    actual = Game.new(Board.new, Player.new("X"), Player.new("O"))
+             |> Game.switch_players()
+             |> (fn game -> game.current_player.marker end).()
+    assert actual == expected
+  end
+
+  test "Game.update_board/2 returns a new Game struct with the board field updated to contain the last move" do
+    expected = "X"
+    actual = Game.new(Board.new, Player.new("X"), Player.new("O"))
+             |> Game.update_board(1)
+             |> (fn game -> game.board[1] end).()
     assert actual == expected
   end
 
